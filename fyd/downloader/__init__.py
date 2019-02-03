69ff5e78be181a76bf256dbeedd6d94c2fe8c2fb
ben <github@jianmin.ninja>
2019-02-03 02:58:28 +0000

    better logging

--- fyd/downloader/__init__.py
+++ fyd/downloader/__init__.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os, sys, time
 import threading
 import pytube
+import traceback
 class downloader:
   interval = 30
   purge = 30
@@ -25,7 +26,8 @@ class downloader:
         f.close()
         os.remove(self.dirs["queue"] + "/" + v)
         self.do_work(v)
-    except:
+    except Exception:
+      traceback.print_exc()
       self.dbg("error iterating queue")
       
   def do_work(self,v):
@@ -40,11 +42,13 @@ class downloader:
       f.close()
       os.remove(self.dirs["working"] + "/" + v)
       self.dbg("done with " + str(v)) 
-    except:
+    except Exception:
+      traceback.print_exc()
       self.dbg("error on this go " + str(v)) 
       try:
         os.remove(self.dirs["working"] + "/" + v) 
-      except:
+      except Exception:
+        traceback.print_exc()
         self.dbg("error removing working file")
       
   def clean_up(self):
@@ -73,7 +77,8 @@ class downloader:
           self.dbg("purging " + str(v))
           os.remove(self.dirs["done"] + "/" + v)
           os.remove(self.dirs["downloads"] + "/" + v + ".mp4")
-    except: 
+    except Exception:
+      traceback.print_exc()
       self.dbg("error on this clean")
 
   def start(self):
